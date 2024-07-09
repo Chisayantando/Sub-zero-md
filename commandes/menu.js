db5072f83d0f9a41dbfc359d0165a09ae208f922
Takudzwa-Mlambo <157640942+Cod3Uchiha@users.noreply.github.com>
2024-07-09 18:48:29 +0200

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -1,218 +1,98 @@
 const util = require('util');
-
 const fs = require('fs-extra');
-
 const { zokou } = require(__dirname + "/../framework/zokou");
-
 const { format } = require(__dirname + "/../framework/mesfonctions");
-
 const os = require("os");
-
 const moment = require("moment-timezone");
-
 const s = require(__dirname + "/../set");
 
-
-
 zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
-
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-
-    let { cm } = require(__dirname + "/../framework//zokou");
-
+    let { ms, repondre, prefixe, nomAuteurMessage, mybotpic } = commandeOptions;
+    let { cm: commandList } = require(__dirname + "/../framework/zokou");
     var coms = {};
-
     var mode = "public";
-
-    
-
-    if ((s.MODE).toLocaleLowerCase() != "yes") {
-
+    if ((s.MODE).toLowerCase() !== "yes") {
         mode = "private";
-
     }
-
-
-
-
-
-    
-
-
-
-    cm.map(async (com, index) => {
-
+    commandList.map(async (com, index) => {
         if (!coms[com.categorie])
-
             coms[com.categorie] = [];
-
         coms[com.categorie].push(com.nomCom);
-
     });
-
-
-
     moment.tz.setDefault(s.TZ);
-
-
-
-// Créer une date et une heure en GMT
-
-const temps = moment().format('HH:mm:ss');
-
-const date = moment().format('DD/MM/YYYY');
-
-
-
-  let infoMsg =  `
-
-┏━━━━━━━𒈒𝕿𝕶𝕸-𝖇𝖔𝖙𒈒━━━━━━━┓
-
+    // Create a date and time in GMT
+    const temps = moment().format('HH:mm:ss');
+    const date = moment().format('DD/MM/YYYY');
+    let infoMsg =  `
+┏━━━━━━━❏Sub-zero❏━━━━━━┓
 ┃ ⿻${mode} mode
-
 ┃ ⿻User : ${s.OWNER_NAME}
-
+┣━━❏Sub-zero Info❏ 
 ┃
-
-┣━━❏TKM-Bot info❏
-
-┃
-
-┃ ⿻Library : Baileys
-
+┃ ⿻Library : WS Baileys
 ️┃ ⿻Prefix : ${s.PREFIXE}
-
 ️┃ ⿻Date : ${date}
-
 ┃ ⿻Time : ${temps}
-
-┃ ⿻Tools : ${cm.length}
-
+┃ ⿻Tools : ${commandList.length}
 ┃ ⿻Ram : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-
 ┃ ⿻Host : ${os.platform()}
-
-┃
-
-┣━━━━━━━𒈒𝕿𝕶𝕸-𝖇𝖔𝖙𒈒━━━➠
-
-┗━━━━━━━𒈒𝖛.²-𝕭𝖚𝖌𒈒━━━━━━━┛\n\n`;
-
-    
-
-let menuMsg = `
-
-┏━━━━━━━━━━━━━━━━━━━━━━┓
-
-┣❏𝕿𝕶𝕸-𝖇𝖔𝖙 𝖛²
-
-┣❏©𝕮𝖔𝖉𝕰𝖀𝖈𝖍𝖎𝖍𝖆
-
-┗━━━━━━━━━━━━━━━━━━━━━━┛
-
-
-
-𒈒𝕿𝕶𝕸-𝕭𝖔𝖙 𝕮𝕸𝕯𝖘𒈒
-
+┣━━━━━━━❏Sub-zero❏━━━━━━┓
+┗━━━━━━━❏Version1❏━━━━━━┛\n\n`;
+    let menuMsg = `
+┏━━━━━━━━━━┓
+┣❏Sub-zero
+┣❏©Ntando
+┗━━━━━━━━━━┛
 `;
-
-
-
+    // Random Bible verses
+    const bibleVerses = [
+        "For I know the plans I have for you, declares the Lord, plans for welfare and not for evil, to give you a future and a hope. - Jeremiah 29:11",
+        "The Lord is my shepherd; I shall not want. - Psalm 23:1",
+        "I can do all things through him who strengthens me. - Philippians 4:13",
+        // Add more Bible verses here
+    ];
+    const randomVerse = bibleVerses[Math.floor(Math.random() * bibleVerses.length)];
+    menuMsg += `Bible Verse: ${randomVerse}\n\n`;
     for (const cat in coms) {
-
         menuMsg += `┏━━━━━⚼ ${cat}`;
-
         for (const cmd of coms[cat]) {
-
             menuMsg += `
-
-┃➠ ${cmd}`;
-
+┣ ⃟  ${cmd}`;
         }
-
         menuMsg += `
-
 ┗━━━━━━━━━━━━━━┛\n`
-
     }
-
-
-
     menuMsg += `
-
-            
-
 ︎┏━━━━━━━━━━━━━━━━━━━━━━┓
-
-️┣❏𝕿𝕶𝕸-𝖇𝖔𝖙 𝖛²
-
-┣❏©𝕮𝖔𝖉𝕰𝖀𝖈𝖍𝖎𝖍𝖆
-
+️┣❏Sub-zero❏
+┣❏©Ntando
 ┗━━━━━━━━━━━━━━━━━━━━━━┛
-
 ┏━━━━━━━━━━━━━━━━━━━━━━┓
-
-┃➠Repo: https://Github.com/Cod3Uchiha/TKM-bot
-
 ┗━━━━━━━━━━━━━━━━━━━━━━┛
-
 `;
 
+    var lien = mybotpic();
 
-
-   var lien = mybotpic();
-
-
-
-   if (lien.match(/\.(mp4|gif)$/i)) {
-
-    try {
-
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *TKM-BOT*, déveloper Cod3uchiha" , gifPlayback : true }, { quoted: ms });
-
+    if (lien.match(/\.(mp4|gif)$/i)) {
+        try {
+            zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis _subzero_, développer Ntando" , gifPlayback : true }, { quoted: ms });
+        }
+        catch (e) {
+            console.log("🥵🥵 Menu erreur " + e);
+            repondre("🥵🥵 Menu erreur " + e);
+        }
+    } 
+    // Vérification pour .jpeg ou .png
+    else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+        try {
+            zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis _subzero_, développer Ntando" }, { quoted: ms });
+        }
+        catch (e) {
+            console.log("🥵🥵 Menu erreur " + e);
+            repondre("🥵🥵 Menu erreur " + e);
+        }
+    } 
+    else {
+        repondre(infoMsg + menuMsg);
     }
-
-    catch (e) {
-
-        console.log("🥵🥵 Menu erreur " + e);
-
-        repondre("🥵🥵 Menu erreur " + e);
-
-    }
-
-} 
-
-// Vérification pour .jpeg ou .png
-
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
-
-    try {
-
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *TKM-bot*, déveloper cod3uchiha" }, { quoted: ms });
-
-    }
-
-    catch (e) {
-
-        console.log("🥵🥵 Menu erreur " + e);
-
-        repondre("🥵🥵 Menu erreur " + e);
-
-    }
-
-} 
-
-else {
-
-    
-
-    repondre(infoMsg + menuMsg);
-
-    
-
-}
-
-
-
 });
-
